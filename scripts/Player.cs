0336c9a4b7750d8d07e5d2e743a90a1073e5856b
Dominik 'dreamsComeTrue' Jasiński <dominikjasinski@o2.pl>
2020-06-21 10:56:00 +0200

    Making Player rotate towards movement direction

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,62 +3,104 @@ using System;
 
 public class Player : KinematicBody
 {
-	private const float MOVE_SPEED = 1.1f;
+    private const float MOVE_SPEED = 1.1f;
 
-	private SpotLight flashLight;
-	private MeshInstance mesh;
+    private SpotLight flashLight;
+    private MeshInstance mesh;
 
-	private float deltaAccumulator = 0.0f;
-	private float floatInitialHeight;
-	private float floatAmplitude = 0.02f;
-	private float floatFrequency = 120.0f;
+    private Vector3 movementDirection = Vector3.Zero;
+    private Vector3 lastDirection = Vector3.Zero;
 
-	public override void _Ready()
-	{
-		flashLight = GetNode<SpotLight>("FlashLight");
-		mesh = GetNode<MeshInstance>("Mesh");
-		floatInitialHeight = mesh.Translation.y;
-	}
+    private float angleAccumulator = 0.0f;
 
-	public override void _Process(float delta)
-	{
-		Vector3 newTranslation = this.Translation;
+    private float deltaAccumulator = 0.0f;
+    private float floatInitialHeight;
+    private float floatAmplitude = 0.02f;
+    private float floatFrequency = 120.0f;
 
-		if (Input.IsActionPressed("move_forward"))
-		{
-			newTranslation.z -= MOVE_SPEED * delta;
-		}
-		if (Input.IsActionPressed("move_backward"))
-		{
-			newTranslation.z += MOVE_SPEED * delta;
-		}
-		if (Input.IsActionPressed("move_left"))
-		{
-			newTranslation.x -= MOVE_SPEED * delta;
-		}
-		if (Input.IsActionPressed("move_right"))
-		{
-			newTranslation.x += MOVE_SPEED * delta;
-		}
+    public override void _Ready()
+    {
+        flashLight = GetNode<SpotLight>("FlashLight");
+        mesh = GetNode<MeshInstance>("Mesh");
+        floatInitialHeight = mesh.Translation.y;
+    }
 
-		if (Input.IsActionJustPressed("action_tool"))
-		{
-			flashLight.Visible = !flashLight.Visible;
-		}
+    public override void _Process(float delta)
+    {
+        movementDirection = Vector3.Zero;
 
-		this.Translation = newTranslation;
+        if (Input.IsActionPressed("move_forward"))
+        {
+            movementDirection.z -= 1.0f;
+        }
+        if (Input.IsActionPressed("move_backward"))
+        {
+            movementDirection.z += 1.0f;
+        }
+        if (Input.IsActionPressed("move_left"))
+        {
+            movementDirection.x -= 1.0f;
+        }
+        if (Input.IsActionPressed("move_right"))
+        {
+            movementDirection.x += 1.0f;
+        }
 
-		UpdateMeshFloat(delta);
-	}
+        if (Input.IsActionJustPressed("action_tool"))
+        {
+            flashLight.Visible = !flashLight.Visible;
+        }
 
-	private void UpdateMeshFloat(float delta)
-	{
-		deltaAccumulator += delta;
-		
-		Vector3 newTranslation = mesh.Translation;
+        this.Translation += movementDirection.Normalized() * delta;
 
-		newTranslation.y = floatInitialHeight;
-		newTranslation.y += Mathf.Sin(Mathf.Deg2Rad(deltaAccumulator * (float)Math.PI * floatFrequency)) * floatAmplitude;
-		mesh.Translation = newTranslation;
-	}
+        _UpdateMeshFloat(delta);
+        _FaceMeshToDirection(delta);
+    }
+
+    private void _UpdateMeshFloat(float delta)
+    {
+        deltaAccumulator += delta;
+
+        Vector3 newTranslation = mesh.Translation;
+
+        newTranslation.y = floatInitialHeight;
+        newTranslation.y += Mathf.Sin(Mathf.Deg2Rad(deltaAccumulator * (float)Math.PI * floatFrequency)) * floatAmplitude;
+        mesh.Translation = newTranslation;
+    }
+
+    private void _FaceMeshToDirection(float delta)
+    {
+        Vector3 directionNormalized = movementDirection.Normalized();
+
+        if (directionNormalized.Length() >= 0.001f)
+        {
+            float currentRotationDegress = (mesh.RotationDegrees.y % 360.0f);
+            float targetAngle = Mathf.Atan2(directionNormalized.x, directionNormalized.z);
+            float targetAngleDegress = (Mathf.Rad2Deg(targetAngle) % 360.0f);
+
+            if (targetAngleDegress < 0)
+            {
+                targetAngleDegress = 360.0f + targetAngleDegress;
+            }
+
+            if (Mathf.IsEqualApprox(currentRotationDegress, targetAngleDegress, 0.1f) || angleAccumulator > 1.0f
+            || lastDirection != directionNormalized)
+            {
+                angleAccumulator = 0.0f;
+            }
+            else
+            {
+                float angle = Mathf.LerpAngle(mesh.Rotation.y, targetAngle, angleAccumulator);
+
+                angleAccumulator += delta * 0.4f;
+                mesh.Rotation = new Vector3(0.0f, angle, 0.0f);
+            }
+
+            // GD.Print("A > " + currentRotationDegress);
+            // GD.Print("B > " + targetAngleDegress);
+            // GD.Print("C > " + angleAccumulator);
+
+            lastDirection = directionNormalized;
+        }
+    }
 }
